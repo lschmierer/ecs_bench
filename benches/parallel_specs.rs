71956cb4c22cb98c66a991ef4049f653a1b144c2
Aceeri <conmcclusk@gmail.com>
2017-07-07 12:25:28 -0700

    Update specs

--- benches/parallel_specs.rs
+++ benches/parallel_specs.rs
@@ -7,26 +7,46 @@ extern crate specs;
 
 extern crate ecs_bench;
 
-use specs::{World, Entity, Component, Gate, Planner, VecStorage};
+use specs::{World, Entity, Component, DenseVecStorage, Dispatcher, DispatcherBuilder, Join, ReadStorage, System, WriteStorage};
 
 use ecs_bench::parallel::{R, W1, W2, N};
 
 struct RComp(R);
 impl Component for RComp {
-    type Storage = VecStorage<RComp>;
+    type Storage = DenseVecStorage<RComp>;
 }
 
 struct W1Comp(W1);
 impl Component for W1Comp {
-    type Storage = VecStorage<W1Comp>;
+    type Storage = DenseVecStorage<W1Comp>;
 }
 
 struct W2Comp(W2);
 impl Component for W2Comp {
-    type Storage = VecStorage<W2Comp>;
+    type Storage = DenseVecStorage<W2Comp>;
 }
 
-fn build() -> Planner<()> {
+struct Sys1;
+impl<'a> System<'a> for Sys1 {
+    type SystemData = (WriteStorage<'a, W1Comp>, ReadStorage<'a, RComp>);
+    fn run(&mut self, (mut w1s, rs): Self::SystemData) {
+        for (w1, r) in (&mut w1s, &rs).join() {
+            w1.0.x = r.0.x;
+        }
+    }
+}
+
+struct Sys2;
+impl<'a> System<'a> for Sys2 {
+    type SystemData = (WriteStorage<'a, W2Comp>, ReadStorage<'a, RComp>);
+    fn run(&mut self, (mut w2s, rs): Self::SystemData) {
+        for (w2, r) in (&mut w2s, &rs).join() {
+            w2.0.x = r.0.x;
+        }
+    }
+}
+
+fn build<'a, 'b>() -> (World, Dispatcher<'a, 'b>) {
     let mut w = World::new();
     w.register::<RComp>();
     w.register::<W1Comp>();
@@ -36,9 +56,9 @@ fn build() -> Planner<()> {
     {
         let ents: Vec<Entity> = w.create_iter().take(N).collect();
 
-        let mut rs = w.write::<RComp>().pass();
-        let mut w1s = w.write::<W1Comp>().pass();
-        let mut w2s = w.write::<W2Comp>().pass();
+        let mut rs = w.write::<RComp>();
+        let mut w1s = w.write::<W1Comp>();
+        let mut w2s = w.write::<W2Comp>();
 
         for e in ents {
             rs.insert(e, RComp(R { x: 0.0 }));
@@ -47,7 +67,11 @@ fn build() -> Planner<()> {
         }
     }
 
-    Planner::new(w)
+    let dispatcher = DispatcherBuilder::new()
+        .add(Sys1, "sys1", &[])
+        .add(Sys2, "sys2", &[])
+        .build();
+    (w, dispatcher)
 }
 
 #[bench]
@@ -57,11 +81,9 @@ fn bench_build(b: &mut Bencher) {
 
 #[bench]
 fn bench_update(b: &mut Bencher) {
-    let mut planner = build();
-
+    let (mut world, mut dispatcher) = build();
+    
     b.iter(|| {
-        planner.run1w1r(|w1: &mut W1Comp, r: &RComp| w1.0.x = r.0.x);
-        planner.run1w1r(|w2: &mut W2Comp, r: &RComp| w2.0.x = r.0.x);
-        planner.wait();
+        dispatcher.dispatch(&mut world.res);
     });
 }
