05259eaa97b60a6956326d282f6d4379e431acfb
Aceeri <conmcclusk@gmail.com>
2017-07-08 17:43:14 -0700

    Call gnuplot

--- bench_results.py
+++ bench_results.py
@@ -5,6 +5,9 @@ benches = ['ecs', 'specs', 'recs', 'trex', 'calx_ecs', 'froggy', 'constellation'
 bench_targets = ['pos_vel', 'parallel']
 bench_names = ['build', 'update']
 
+libraries = ' '.join(benches)
+print libraries
+
 def parse(out, bench_target, bench_name):
     index_target = out.index(bench_target)
     index_bench = out.index(bench_name, index_target)
@@ -20,9 +23,22 @@ with open('README.md.tmpl', 'r') as f:
 
 for target in bench_targets:
     for name in bench_names:
-        for bench in benches:
+        target_name = target + "_" + name
+
+        for i, bench in enumerate(benches):
             result = parse(out, target + '_' + bench, 'bench_' + name)
+            with open("data/" + target_name, 'w') as dat:
+                dat.write("{} {} {} 0.0", bench, i * 15, result)
+
             readme = readme.replace('{' + target + '_' + name + '_' + bench + '}', result)
+
+        args = "\"bench='{}';data='data/{}';libraries='{}';\""
+        args.format(target_name.replace('_', ' '), target_name, libraries);
+
+        subprocess.call(
+            ["gnuplot", "-e", args, "bar.plot", ">", target_name + ".svg"],
+            stderr=subprocess.STDOUT
+        )
 
 with open('README.md', 'w') as f:
     f.write(readme)
